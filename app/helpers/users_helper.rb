e39c4ab8f8843fa6616e24426803ed8fcc54fbb7
Brian Bragg <brian.c.bragg@gmail.com>
2013-01-15 08:08:22 -0500

    layout changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,12 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{default_gravatar}"
     image_tag(gravatar_url, alt: user.email, class: "gravatar")
   end
+
+  def gravatar_for_large(user, options = { size: 50 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    default_gravatar = URI.encode_www_form_component('https://businessleads.com/images/avatar_placeholder.jpg')
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{default_gravatar}"
+    image_tag(gravatar_url, alt: user.email)
+  end
 end
