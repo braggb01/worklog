aea148bb4d3356571780d4bb65e04a731bec756d
Brian Bragg <brian.bragg@mssm.edu>
2013-01-16 13:26:40 -0500

    sort jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.paginate(page: params[:page]).per_page(25)
+    @jobs = Job.order("install_date DESC").paginate(page: params[:page]).per_page(25)
 
     respond_to do |format|
       format.html # index.html.erb
